dd34be87718b6998e7866b50c9634ad677783cfe
nessKenya <nelson.muchonji@gmail.com>
2024-09-25 15:38:47 +0300

    dynamic product rendering

--- src/js/product.js
+++ src/js/product.js
@@ -1,32 +1,9 @@
-import { getLocalStorage, setLocalStorage } from './utils.mjs';
+import { getParam } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+import ProductDetails from './productDetails.mjs';
 
 const dataSource = new ProductData('tents');
+const productId = getParam('product')
 
-function addProductToCart(product) {
-  // get cuurent products in cart
-  let products = getLocalStorage('so-cart');
-
-  // confirm if cart is empty
-  if (products) {
-    let newProductList = products.concat(product);
-    setLocalStorage('so-cart', newProductList);
-  } else {
-    /**
-     * so-cart was empty
-     * initialize with new/first product added
-     * should be an array
-     * */
-    setLocalStorage('so-cart', Array(product));
-  }
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();
