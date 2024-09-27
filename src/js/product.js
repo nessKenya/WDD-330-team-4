f3772ecc3015143b95752eaa7bdf3400e53d04c9
Adedeji234501 <Azeezadedeji1k@gmail.com>
2024-09-27 17:20:10 -0400

    aaindividual

--- src/js/product.js
+++ src/js/product.js
@@ -5,8 +5,8 @@ import ProductData from './ProductData.mjs';
 const dataSource = new ProductData('tents');
 
 //function addProductToCart(product){
-//  setLocalStorage("so-cart", product);
-//}
+//  setLocalStorage("so-cart", product);  
+//} 
 
 function addProductToCart(product) {
   const existingCart = getLocalStorage('addToCart') || [];
