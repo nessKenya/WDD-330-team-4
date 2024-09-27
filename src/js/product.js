edf7708ea181987182c943cfd2f3db5bc35e3225
ameliatuttle <aetuttle@byui.edu>
2024-09-26 23:35:20 -0600

    Add to cart
    Fix the add to cart function. It wasn't storing as an array so I added checks at every point to ensure it was in localStorage as an array.

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,22 @@
-import { setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
+import ProductData from './ProductData.mjs';
 
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // get the cart or create new array for the cart
+  let products = getLocalStorage('so-cart') || [];
+
+  // make sure the cart is in an array!!!
+  if (!Array.isArray(products)) {
+    products = [];
+  }
+
+  // add the product to cart
+  let newProductList = products.concat(product);
+  setLocalStorage('so-cart', newProductList);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
@@ -14,5 +25,5 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+  .getElementById('addToCart')
+  .addEventListener('click', addToCartHandler);
