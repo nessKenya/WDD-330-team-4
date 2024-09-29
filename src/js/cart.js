f13eb5845064290ba589b2aad5b3fa481c8deec2
Adedeji234501 <Azeezadedeji1k@gmail.com>
2024-09-28 21:57:26 -0400

    Remove Cart Items

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,19 @@
-import { getLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage} from './utils.mjs';
 
 function renderCartContents() {
   const cartItems = getLocalStorage('so-cart');
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector('.product-list').innerHTML = htmlItems.join('');
+  if (cartItems === null) {
+    setLocalStorage('so-cart',[]);
+  }else {
+    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+    document.querySelector('.product-list').innerHTML = htmlItems.join('');
+  }
+  document.querySelectorAll('.cart-card__remove').forEach(element => {
+    element.addEventListener('click', () => {
+      const itemId = element.getAttribute('data-id');
+        removeCartItem(itemId);
+    });
+  });
 }
 
 function cartItemTemplate(item) {
@@ -20,9 +30,19 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <span class="cart-card__remove" data-id=${item.Id}>❌</span>
 </li>`;
 
   return newItem;
 }
 
 renderCartContents();
+function removeCartItem(idToRemove) {
+  const cartItems = getLocalStorage('so-cart');
+  const index = cartItems.findIndex(item => item.Id === idToRemove);
+  if (index !== -1) {
+    cartItems.splice(index, 1);
+  }
+  setLocalStorage('so-cart',cartItems);
+  renderCartContents();
+}
