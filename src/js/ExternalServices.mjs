313bae9ff5840ffc2aa80117b7798a23ec36eaca
ameliatuttle <aetuttle@byui.edu>
2024-10-15 01:43:55 -0600

    .env and images fix

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,36 +1,32 @@
-const baseURL = 'http://server-nodejs.cit.byui.edu:3000/';
+const baseURL = import.meta.env.VITE_SERVER_URL
 async function convertToJson(res) {
-  const data = await res.json();
+  const jsonResponse = await res.json();
   if (res.ok) {
-    return data;
+    return jsonResponse;
   } else {
-    throw { name: 'servicesError', message: data };
+    throw { name: 'servicesError', message: jsonResponse };
   }
 }
-
 export default class ExternalServices {
-  constructor(category) {
-    // this.category = category;
-    // this.path = `../json/${this.category}.json`;
-  }
   async getData(category) {
     const response = await fetch(baseURL + `products/search/${category}`);
     const data = await convertToJson(response);
     return data.Result;
   }
   async findProductById(id) {
-    const response = await fetch(baseURL + `product/${id}`);
-    const data = await convertToJson(response);
-    return data.Result;
+    const response = await fetch(`${baseURL}product/${id}`);
+    const productData = await convertToJson(response);
+    return productData.Result;
   }
   async checkout(payload) {
+    const url = `${baseURL}/checkout`;
     const options = {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(payload),
-    };
-    return await fetch(baseURL + 'checkout/', options).then(convertToJson);
+      body: JSON.stringify(payload)
+    }
+    return await fetch(url, options).then(convertToJson);
   }
 }
